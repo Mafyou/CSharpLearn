86575e0a08e1464a7cb79111213f0c9da8fc1828
Mafyou <mdupleix@hotmail.com>
2025-01-18 10:52:02 +0100

    Add Benchmamrks to compare

--- AsyncAwait/TaskToWaitFor.cs
+++ AsyncAwait/TaskToWaitFor.cs
@@ -1,5 +1,4 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
 
 namespace AsyncAwait;
 
@@ -14,10 +13,56 @@ public class TaskToWaitFor
     }
 
     [Benchmark]
-    public Task<Task> RunItFromResult()
+    public async Task<bool> RunItFireAndForget()
     {
         var hasWaited = false;
+        _ = Task.Delay(TimeSpan.FromSeconds(5));
+        return hasWaited;
+    }
+
+    [Benchmark]
+    public Task<Task> RunItFromResult()
+    {
         var res = Task.FromResult(Task.Delay(TimeSpan.FromSeconds(5)));
         return res;
+    }
+
+    [Benchmark]
+    public async Task<bool> RunItAwaitTaskRunFromResult()
+    {
+        var hasWaited = false;
+        await Task.Run(() => Task.FromResult(Task.Delay(TimeSpan.FromSeconds(5))));
+        return hasWaited;
+    }
+    [Benchmark]
+    public async Task<bool> RunItTaskFireAndForget()
+    {
+        var hasWaited = false;
+        _ = Task.Run(() => Task.FromResult(Task.Delay(TimeSpan.FromSeconds(5))));
+        return hasWaited;
+    }
+
+    [Benchmark]
+    public async Task<bool> RunItAwaitTaskRun()
+    {
+        var hasWaited = false;
+        await Task.Run(() => Task.Delay(TimeSpan.FromSeconds(5)));
+        return hasWaited;
+    }
+
+    [Benchmark]
+    public async Task<bool> RunItAwaitTaskRunInnerAwait()
+    {
+        var hasWaited = false;
+        await Task.Run(async () => await Task.Delay(TimeSpan.FromSeconds(5)));
+        return hasWaited;
+    }
+
+    [Benchmark]
+    public async Task<bool> RunItTaskRunAwaitInnerAwait()
+    {
+        var hasWaited = false;
+        await Task.Run(async () => await Task.Delay(TimeSpan.FromSeconds(5)));
+        return hasWaited;
     }
 }
